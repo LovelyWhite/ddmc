ed0e05dc8235ee7efafe21667f2a207bdb33cdd3
wink <icezxn@qq.com>
2022-04-10 00:24:50 +0800

    utils,model: support weapp #8

--- src/main/java/utils/Configs.java
+++ src/main/java/utils/Configs.java
@@ -1,15 +1,30 @@
 package utils;
 
 public class Configs {
-    public static final String stationId = "***";
-    public static final String addressId = "***";
-    public static final String cityNumber = "0101";
-    public static final Double latitude = 0.0;
-    public static final Double longitude = 0.0;
 
-    public static final String cookie = "***";
-    public static final String uid = "***";
-    public static final String deviceToken = "***";
+    /**
+     * 默认为给到 APP
+     * 如需设置小程序，请修改：
+     *  appClientId 3 -> 4
+     */
+
+    public static final String stationId = "";
+    public static final String addressId = "";
+    public static final String cityNumber = "0101";
+    public static final Double latitude = 0.00;
+    public static final Double longitude = 0.00;
+    public static final String appClientId = "4"; // 小程序请设置为 4
+
+    public static final String cookie = "";
+    public static final String uid = "";
+    public static final String deviceToken = "";
+
+    /**
+     * 注意，小程序的 api 版本与给到不同
+     * 给到: apiVersion 9.44.0、 appVersion 2.74.2
+     * 小程序: apiVersion 9.49.2、 appVersion 2.82.0
+     */
     public static final String apiVersion = "9.44.0";
     public static final String appVersion = "2.74.2";
+
 }
